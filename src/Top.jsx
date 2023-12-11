70ded82050f3e1f13dc5f7ec8688d44608bf38e8
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-12-11 17:50:56 +0900

    偽名と本名を打つフォームを作成

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
 
-const Top = props => {
-  const [Rooms,setRooms] = useState([]);
-  
-  // スタートしたらApp.jsxのnameに値をセットする(props.setName)
+const Top = (props) => {
+  const [Rooms, setRooms] = useState([]);
+  const [pseudo, setPseudo] = useState(""); //偽名
+  const [real, setReal] = useState(""); //本名
+
+  // スタート時App.jsxのnameに値をセットする(props.setName)
 
   return (
     <div>
+      <span>偽名:</span>
+      <input
+        type="text"
+        id="pseudo"
+        value={pseudo}
+        onChange={(e) => {
+          setPseudo(e.target.value);
+        }}
+      />
+      <span>本名:</span>
+      <input
+        type="text"
+        id="real"
+        value={real}
+        onChange={(e) => {
+          setReal(e.target.value);
+        }}
+      />
       <button>create</button>
-      {Rooms.map((room,index)=>{
+      {Rooms.map((room, index) => {
         return <ARoom key={index} {...room} />;
       })}
     </div>
@@ -21,8 +41,8 @@ const ARoom = (props) => {
       <h1>{props.name}</h1> {/* 部屋の名前 */}
       <p>{props.num}/5</p> {/* 部屋にいる人数 */}
       <ul>
-        {props.members.map((member,index) => {
-            return <li key={index}>{member}</li>;
+        {props.members.map((member, index) => {
+          return <li key={index}>{member}</li>;
         })}
       </ul>
       {props.isHost ? <button>START</button> : null}{" "}
