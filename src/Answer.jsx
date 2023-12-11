4de792bbbdfa057db92ea2169cce647faeef3a5d
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-12-11 18:37:44 +0900

    コメントの修正、画像の表示

--- src/Answer.jsx
+++ src/Answer.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
 const Answer = (props) => {
-  const answer = {}; //答えの人のアイコン、偽名、本名、IDが入ってる
+  const answer = {}; //答えの人のアイコン(answer.image)、偽名(answer.pseudo)、本名(answer.real)、ID(answer.id)が入ってる
 
   return (
     <div>
       <h1>
         {answer.id === props.choice ? "正解!!正解!!正解!!正解!!" : "不正解..."}
       </h1>
+      <img src={answer.image} alt={answer.real} />
       <p>
         {answer.pseudo}さんの正体は{answer.real}さんでした
       </p>
