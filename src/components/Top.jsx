e784654520e629f33cf87a3381121c64538ec9df
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-12-12 18:50:58 +0900

    部屋の作成フォーム作り切りました。

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -5,6 +5,7 @@ const Top = (props) => {
   const [pseudo, setPseudo] = useState(""); //偽名
   const [real, setReal] = useState(""); //本名
   const [open, setOpen] = useState(false);
+  const [rname, setRname] = useState("");
 
   // スタート時すべてのユーザーが自身のApp.jsxのselfに自身のデータをセットする(props.setSelf)
   // 後でチャットするときに自分のデータが必要になるため(idや名前、アイコンなど)
@@ -29,21 +30,32 @@ const Top = (props) => {
         onChange={(e) => {
           setReal(e.target.value);
         }}
+        disabled={open}
       />
-      <button onClick={()=>{
-        setOpen(true);
-      }}>create</button> {/**部屋の作成 */}
-      <div id="create_form">
+      <button
+        onClick={() => {
+          setOpen(true);
+          //部屋名や自分のデータと一緒にサーバーに送信
+        }}
+      >
+        create
+      </button>
+      {/**部屋の作成 */}
         <span>部屋名:</span>
         <input
           type="text"
           id="roomName"
-          value={pseudo}
+          disabled={open}
+          value={rname}
           onChange={(e) => {
-            setPseudo(e.target.value);
+            setRname(e.target.value);
           }}
         />
-      </div>
+      <button disabled={!open} onClick={()=>{
+        setOpen(false);
+        
+        //部屋を閉じる処理
+      }}>部屋を閉じる</button>
       {Rooms.map((room, index) => {
         return <ARoom key={index} {...room} />;
       })}
@@ -56,7 +68,9 @@ const ARoom = (props) => {
     <div>
       <h1>{props.name}</h1> {/* 部屋の名前 */}
       <p>{props.num}/5</p> {/* 部屋にいる人数 */}
-      <ul> {/**部屋にいる人たち */}
+      <ul>
+        {" "}
+        {/**部屋にいる人たち */}
         {props.members.map((member, index) => {
           return <li key={index}>{member}</li>;
         })}
