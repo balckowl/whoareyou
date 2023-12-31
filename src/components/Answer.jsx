8a2e90faeba94f70f383d74ca1d8f71be088c907
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-12-31 12:45:18 +0900

    答えのページに移るようにできました。また、自分の名前を問題から省く処理もできました。

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 const Answer = (props) => {
-  const answer = {}; //答えの人のアイコン(answer.image)、偽名(answer.pseudo)、本名(answer.real)、ID(answer.id)が入ってる
+  // const answer = {}; //答えの人のアイコン(answer.image)、偽名(answer.pseudo)、本名(answer.real)、ID(answer.id)が入ってる
 
   return (
     <div>
       <h1>
-        {answer.id === props.choice ? "正解!!正解!!正解!!正解!!" : "不正解..."}
+        {props.correct ? "正解!!正解!!正解!!正解!!" : "不正解..."}
       </h1>
-      <img src={answer.image} alt={answer.real} />
+      <img src={props.answer.image} alt={props.answer.real} />
       <p>
-        {answer.pseudo}さんの正体は{answer.real}さんでした
+        {props.answer.pseudo}さんの正体は{props.answer.real}さんでした
       </p>
-      <button onClick={() => props.Back()}>戻る</button>
+      {/* <button onClick={() => props.Back()}>戻る</button> */}
     </div>
   );
 };
