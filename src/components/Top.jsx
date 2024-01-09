a17e8513b372512b3639f844ac4c4d65abd7090c
Sakana <45968398+Sakana-2@users.noreply.github.com>
2024-01-09 20:21:20 +0900

    Linkをbuttonの外側に出しました

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -4,14 +4,13 @@ import "./styles/Top.css";
 const Top = (props) => {
   return (
     <div className="button-class">
-      <button>
-        <Link to="/roommake">部屋を作る</Link >
-      </button>
-      <button onClick={()=>{
-        localStorage.setItem("maker",0);//ルーム作成者なら1,参加者なら0
-      }}>
-        <Link to="/roomjoin">部屋を探す</Link>
-      </button>
+        <Link to="/roommake"><button>部屋を作る</button></Link >
+        <Link to="/roomjoin">
+          <button onClick={()=>{
+            localStorage.setItem("maker",0);//ルーム作成者なら1,参加者なら0
+          }}>部屋を探す
+          </button>
+        </Link>
     </div>
   );
 };
