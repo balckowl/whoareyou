72c1f8d072e58f2716c3326b29b16d4697e7426a
AIM-KAmi0 <a5120058@ima.aim.aoyama.ac.jp>
2024-01-11 17:41:59 +0900

    update: localstorageのデータを削除し、アニメーションを設定しました。

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import "./styles/Top.css";
+import { useEffect } from "react";
+import { motion } from "framer-motion"
 
 const Top = (props) => {
+
   return (
     <div className="button-class">
-        <Link to="/roommake"><button>部屋を作る</button></Link >
-        <Link to="/roomjoin">
-          <button onClick={()=>{
-            localStorage.setItem("maker",0);//ルーム作成者なら1,参加者なら0
-          }}>部屋を探す
-          </button>
-        </Link>
+      <Link to="/roommake"><motion.button whileHover={{ scale: 1.1 }}>部屋を作る</motion.button></Link >
+      <Link to="/roomjoin">
+        <motion.button whileHover={{ scale: 1.1 }} onClick={() => {
+          localStorage.setItem("maker", 0);//ルーム作成者なら1,参加者なら0
+        }}>部屋を探す
+        </motion.button>
+      </Link>
     </div>
   );
 };
